2a3fe0cba46eda9dd024c6148dea1554d997ac3d
unknown <sajutidey19@gmail.com>
2020-12-19 16:04:51 +0600

    ForTheBeloved

---
+++ js/js.js
@@ -0,0 +1,15 @@
+function click_me(){
+    var a= document.getElementById("two");
+    if(a.style.display==="block")
+    {a.style.display="none";}
+    else{a.style.display="block";}
+}
+
+
+// function show_hide(){
+//     var a= document.getElementById("menu");
+//     if(a.style.display==="block")
+//     {a.style.display="none";}
+//     else{a.style.display="block";}
+// }
+
