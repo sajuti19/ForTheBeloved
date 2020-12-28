a2439d35236abadb3012c55f21a7e3e350990438
unknown <sajutidey19@gmail.com>
2020-12-28 22:34:16 +0600

    ForAna

--- js/js.js
+++ js/js.js
@@ -4,12 +4,14 @@ function click_me(){
     {a.style.display="none";}
     else{a.style.display="block";}
 }
+function play() {
+    var audio = document.getElementById("audio");
+    audio.play();
+  }
+  function pause() {
+    var audio = document.getElementById("audio");
+    audio.pause();
+  }
 
 
-// function show_hide(){
-//     var a= document.getElementById("menu");
-//     if(a.style.display==="block")
-//     {a.style.display="none";}
-//     else{a.style.display="block";}
-// }
 
